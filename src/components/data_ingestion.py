1501c5db220501c2f5cbd9039b78286eed9f6095
Minhal Haider <minhalhaider97@gmail.com>
2026-07-21 20:18:04 +0500

    predict_pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,9 @@ from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
     test_data_path: str=os.path.join('artifacts', "test.csv")
@@ -56,5 +59,9 @@ if __name__ == '__main__':
     train_path, test_path, = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr=train_arr, test_arr=test_arr))
+
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ import dill
 import pickle
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import r2_score 
 
 def save_object(file_path, obj):
     try:
@@ -18,3 +21,41 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+
+def evaluate_models(X_train,y_train, X_test, y_test, models, params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
+            model.fit(X_train, y_train) #training model
+
+            predicted_y_train = model.predict(X_train)
+            predicted_y_test = model.predict(X_test)
+
+            test_model_score = r2_score(y_test , predicted_y_test)
+            train_model_score = r2_score(y_train , predicted_y_train)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+
